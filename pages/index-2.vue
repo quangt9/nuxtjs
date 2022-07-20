28ab0981cdce39dbe29f9c314910d2f652cafe05
quangt9 <quangt9gr7x4@gmail.com>
2022-07-20 21:35:32 +0700

    abc2

---
+++ pages/index-2.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Dashboard</h1>
+sdasadssssssssssss
+      <!-- <p>Hi {{ user.name }}</p> -->
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name : 'indexPage',
+  data() {
+    return {
+      user: this.$auth.user,
+    }
+  }
+}
+</script>
