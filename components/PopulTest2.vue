bbf10acfbd317887f93c167b20347043a318a573
quangt9 <quangt9gr7x4@gmail.com>
2022-07-20 21:07:23 +0700

    test stash

---
+++ components/PopulTest2.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="modal-overlay">
+    <div class="close-button" @click="closePopul">sưdeasd</div>
+    <div class="popul-modal">
+        <div class="close-button" @click="closePopul">sưdeasd</div>
+        <div class="modal-content">
+                aliceblu
+                <h5 @click="closePopul">xzy</h5>
+            </div>     
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    methods: {
+        closePopul() {
+            this.$emit('close-modal')
+        }
+    },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/home1.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <popul-test v-show="show" @close-modal="changeShow" />
+      <h3>ok e</h3>
+    <div class="save-btn">
+        <button @click="changeShow()">Save</button>
+    </div>
+      <h3>ok e</h3>
+</div>
+</template>
+
+<script>
+import PopulTest from '../components/PopulTest.vue'
+export default {
+  components: { PopulTest },
+    data() {
+        return {
+            show: false
+        }
+    },
+    methods: {
+        changeShow() {
+            this.show = !this.show
+        }
+    },
+}
+</script>
